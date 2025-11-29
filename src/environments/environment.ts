6ae42c0aa5507474c4054dddd8ec95476f16d8d0
Eliu Ortega <eliu.orteg@gmail.com>
2025-11-29 15:42:54 -0400

    chore: update firebase configuration to new project
    Update both development and production environment files with new firebase credentials for the saiph-texas project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyDL2WQAyMr4r-drxbPJ8UuHnZOdfue07YY',
-    authDomain: 'saiph-1de28.firebaseapp.com',
-    projectId: 'saiph-1de28',
-    storageBucket: 'saiph-1de28.firebasestorage.app',
-    messagingSenderId: '63064187738',
-    appId: '1:63064187738:web:0ff1964374aa21f857c963',
+    apiKey: 'AIzaSyCRlr-jKw-Z_DvtOZi3-3d30pNWK8R-3qc',
+    authDomain: 'saiph-texas.firebaseapp.com',
+    projectId: 'saiph-texas',
+    storageBucket: 'saiph-texas.firebasestorage.app',
+    messagingSenderId: '193500766192',
+    appId: '1:193500766192:web:82deeaa55ad772aaac0735',
   },
 };
